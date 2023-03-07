443a8b12b0c420859bab192616b3051e14299fa9
DESKTOP-HVB515S\PhatHuy <huynh_phat_huy@allgrow-labo.jp>
2023-03-07 16:45:36 +0700

    update

--- _public/assets/js/main.js
+++ _public/assets/js/main.js
@@ -2,10 +2,9 @@ const drops = document.querySelector('.c-animation__drops');
 const leaftop = document.querySelector('.c-animation__leaftop');
 const leafright = document.querySelector('.c-animation__leafright');
 
-const imgProcess11 = document.querySelector('.c-listprocess__item:nth-child(1) .c-listProcess__img');
-const imgProcess10 = document.querySelector('.c-listprocess__item:nth-child(2) .c-listProcess__img');
-const imgProcess9 = document.querySelector('.c-listprocess__item:nth-child(3) .c-listProcess__img');
+const boxmore = document.querySelectorAll('.c-boxseffect__more');
 
+const width = document.body.innerWidth;
 var lastScrollTop = 0;
 
 const activeClass = (value) => {
@@ -25,40 +24,64 @@ const activeClass = (value) => {
 
 
 window.addEventListener("scroll", function () {
-    const st = window.pageYOffset || document.documentElement.scrollTop;
+    const st = document.documentElement.scrollTop;
     if (st > lastScrollTop) {
         activeClass(false);
-    } else if (st < lastScrollTop) {
+    } else if (st <= lastScrollTop) {
         activeClass(true);
     }
-    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 }, false);
 
-
-const width = document.body.clientWidth;
-
-window.addEventListener('resize', () => {
-    if (width <= 768) {
-        imgProcess11.setAttribute('src', './assets/img/process11mobile.png');
-        imgProcess10.setAttribute('src', './assets/img/process10mobile.png');
-        imgProcess9.setAttribute('src', './assets/img/process9mobile.png');
-    }
-})
-
-
-
 const iconmenu = document.querySelectorAll('.c-menuicon');
-const navbarmobile = document.querySelector('.c-navbarmobile'); 
+const navbarmobile = document.querySelector('.c-navbarmobile');
 const navbarmbitem = document.querySelectorAll('.c-navbarmobile__item');
 
 iconmenu.forEach((icon) => {
     icon.addEventListener('click', () => {
         navbarmobile.classList.toggle('is-active');
+        document.body.classList.toggle('is-noscroll');
     })
 })
 
 navbarmbitem.forEach((item) => {
     item.addEventListener('click', () => {
         navbarmobile.classList.remove('is-active');
+        document.body.classList.remove('is-noscroll');
+    })
+})
+
+const overlay = document.querySelectorAll('.c-boxseffect__overlay');
+
+boxmore.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        overlay.forEach((over) => {
+            over.classList.remove('is-active');
+        })
+        e.target.offsetParent.querySelector('.c-boxseffect__overlay').classList.add('is-active');
+    })
+})
+
+const phone = document.querySelector('.c-footer__phone');
+
+window.addEventListener('resize', (e) => {
+    if (e.target.innerWidth <= 1025) {
+        phone.setAttribute('href','tel: 06-6231-5626');
+    } else {
+        phone.setAttribute('href','#');
+    }
+})
+
+if (width <= 1025) {
+    phone.setAttribute('href','tel: 06-6231-5626');
+} else {
+    phone.setAttribute('href','#');
+}
+
+const listMoreSp = document.querySelectorAll('.c-listprocess__icon');
+
+listMoreSp.forEach((more) => {
+    more.addEventListener('click', (e) => {
+        e.target.parentElement.querySelector('.c-listprocess__desc').classList.toggle('is-active');
+        e.target.classList.toggle('is-active');
     })
 })
